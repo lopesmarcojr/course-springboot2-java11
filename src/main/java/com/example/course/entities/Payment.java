2e71951bc4e26312ba7f294d760d5238453f5950
MarcoAurelioGit <marcoaurelio.lopes.junior@gmail.com>
2022-06-02 11:50:03 -0300

    Subtotal & Total methods

--- src/main/java/com/example/course/entities/Payment.java
+++ src/main/java/com/example/course/entities/Payment.java
@@ -12,6 +12,8 @@ import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 @Table(name = "tb_paymemt")
 public class Payment implements Serializable {
@@ -25,6 +27,7 @@ public class Payment implements Serializable {
 	
 	@OneToOne
 	@MapsId
+	@JsonIgnore
 	private Order order;
 	
 	public Payment() {
